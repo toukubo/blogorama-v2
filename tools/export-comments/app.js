24dba6c86bf1c864ddcc2f6d77e07bdb8fb79445
Rajasekharan Vengalil <rajave@microsoft.com>
2014-05-11 15:01:35 +0530

    Comleted implementation of exporting comments from blogorama to custom WXR format as required by Disqus.

--- tools/export-comments/app.js
+++ tools/export-comments/app.js
@@ -5,7 +5,11 @@ var Q = require("q");
 var FS = require("fs");
 var sqlite3 = require("sqlite3").verbose();
 var _ = require("lodash");
-var xml = require("xml");
+var xmlbuilder = require("xmlbuilder");
+var sprintf = require("sprintf").sprintf;
+
+// base URL to use in posts
+var baseUrl = "http://ghost-nerd.azurewebsites.net/";
 
 function main() {
     var comments;
@@ -22,39 +26,64 @@ function main() {
             post.comments.push(comment);
         });
 
-        var root = {
-            rss: [
-                {
-                    _attr: {
-                        "version": "2.0",
-                        "xmlns:content": "http://purl.org/rss/1.0/modules/content/",
-                        "xmlns:dsq": "http://www.disqus.com/",
-                        "xmlns:dc": "http://purl.org/dc/elements/1.1/",
-                        "xmlns:wp": "http://wordpress.org/export/1.0/"
-                    }
-                },
-                {
-                    channel: [
-                        {
-                            item: [
-                                {
-                                    title: "Foo"
-                                },
-                                {
-                                    link: "foo.com"
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        };
+        var root = xmlbuilder.create({
+            rss: {
+                "@version": "2.0",
+                "@xmlns:content": "http://purl.org/rss/1.0/modules/content/",
+                "@xmlns:dsq": "http://www.disqus.com/",
+                "@xmlns:dc": "http://purl.org/dc/elements/1.1/",
+                "@xmlns:wp": "http://wordpress.org/export/1.0/"
+            }
+        }, {
+            version: "1.0",
+            encoding: "UTF-8"
+        });
+        var channel = root.element("channel");
 
-        console.log(xml(root, {
-            declaration: true,
-            indent: true
+        // serialize posts
+        _.forEach(posts, function(post) {
+            var item = channel.element({
+                item: {
+                    title: post.title,
+                    link: baseUrl + post.slug,
+                    "dsq:thread_identifier": post.id,
+                    "wp:post_date_gmt": convertDateToDisqusFormat(post.created_at),
+                    "wp:comment_status": "open"
+                }
+            });
+            item.element("content:encoded").dat(post.html);
+
+            // serialize each comment
+            _.forEach(post.comments, function(comment) {
+                var cmt = item.element({
+                    "wp:comment": {
+                        "wp:comment_id": comment.comment_id,
+                        "wp:comment_author": comment.comment_author_name,
+                        "wp:comment_author_email": comment.comment_author_email,
+                        "wp:comment_date_gmt": convertDateToDisqusFormat(new Date(comment.comment_date).getTime()),
+                        "wp:comment_approved": 1
+                    }
+                });
+                cmt.element("wp:comment_content").dat(comment.comment_text);
+            });
+        });
+
+        console.log(root.end({
+            pretty: true,
+            indent: '  '
         }));
     });
+}
+
+function convertDateToDisqusFormat(val) {
+    var date = new Date(val);
+    return sprintf("%d-%02d-%02d %02d:%02d:%02d",
+        date.getUTCFullYear(),
+        date.getUTCMonth(),
+        date.getUTCDate(),
+        date.getUTCHours(),
+        date.getUTCMinutes(),
+        date.getUTCSeconds());
 }
 
 function findPost(posts, slug) {
